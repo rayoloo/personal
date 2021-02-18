62154977ba2e44cb2fde140f34b27b6fd86a4525
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-02-18 14:51:41 -0500

    Initial commit

---
+++ src/Components/PageContent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function PageContent(props){
+    const resumeLink = "https://drive.google.com/u/0/uc?id=1BR-g3xiQqkBeL8NMKDqBhmsODcHQOFxc&export=download"
+    return(
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+
+}
+
+export default PageContent
