057c9b3c3265eb2b205f0764e36c316fbcc54587
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-02-18 19:01:59 -0500

    pls work

--- src/components/WelcomeContent.js
+++ src/components/WelcomeContent.js
@@ -3,13 +3,13 @@ import { Card } from 'antd';
 
 export default function WelcomeContent(){
     return(
-    <>
-          <Card>
+    <div>
+        <Card>
             <h2>Welcome</h2>
             <p>My name is Raymond Lam.</p>
             <p>I have completed a bachelor in computer engineering (B.Eng) at Ryerson University. I am currently seeking job opportunities.</p>
         </Card>
-    </>
+    </div>
     )
 }
 
