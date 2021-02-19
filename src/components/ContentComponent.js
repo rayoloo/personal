ba3fa58c8c1af9d171aa604e798e334a294eec43
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-02-18 19:23:45 -0500

    basic components

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import {Layout} from 'antd'
-import PageContent from "./PageContent"
+import {Layout, Card} from 'antd'
+
 
 export default function ContentComponent() {
     const {Content} = Layout
+    //const resumeLink = "https://drive.google.com/u/0/uc?id=1BR-g3xiQqkBeL8NMKDqBhmsODcHQOFxc&export=download"  
     return (
         <Content>
-            <PageContent/>
+            <Card>
+                <h2>Welcome</h2>
+                <p>My name is Raymond Lam.</p>
+                <p>I have completed a bachelor in computer engineering (B.Eng) at Ryerson University. I am currently seeking job opportunities.</p>
+            </Card>
         </Content>
     )
 }
