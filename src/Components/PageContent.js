9fbad089beddc3f65420a83536c1b083d3aa6825
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-02-18 19:08:58 -0500

    react hosting test

--- src/Components/PageContent.js
+++ src/Components/PageContent.js
@@ -1,17 +1,16 @@
 import React from "react"
-import WelcomeContent from "./WelcomeContent"
-import {Layout} from 'antd'
+import {Layout, Card} from 'antd'
 
 export default function PageContent(props){
     //const resumeLink = "https://drive.google.com/u/0/uc?id=1BR-g3xiQqkBeL8NMKDqBhmsODcHQOFxc&export=download"
     const {Content} = Layout
     
     return(
-        <div>
-                <Content>
-                    <WelcomeContent/>
-                </Content>
-        </div>
+        <Card>
+            <h2>Welcome</h2>
+            <p>My name is Raymond Lam.</p>
+            <p>I have completed a bachelor in computer engineering (B.Eng) at Ryerson University. I am currently seeking job opportunities.</p>
+        </Card>
     )             
 
 }
