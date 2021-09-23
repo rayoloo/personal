ee0efb0b3179dfe701a94f7a18e6e7e4942ff2b0
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-09-22 21:49:46 -0400

    d

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from "react"
+import React from 'react'
 import 'antd/dist/antd.css'
 import './App.css'
-import {Layout} from 'antd'
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-import HeaderComponent from "./components/HeaderComponent"
-import SideBarComponent from "./components/SideBarComponent";
-import FooterComponent from "./components/FooterComponent";
-import HomeComponent from "./components/HomeComponent";
-import ResumeComponent from "./components/ResumeComponent"
+import { Layout } from 'antd'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import HeaderComponent from './components/HeaderComponent'
+import SideBarComponent from './components/SideBarComponent'
+import FooterComponent from './components/FooterComponent'
+import HomeComponent from './components/HomeComponent'
+import ResumeComponent from './components/ResumeComponent'
 import {
-    project1,
-    project2,
-    project3,
-    project4,
-    project5,
-    project6,
-    project7
-    } from "./components/ProjectComponent"
-import ExperienceComponent from "./components/ExperienceComponent"
+	project1,
+	project2,
+	project3,
+	project4,
+	project5,
+	project6,
+	project7,
+} from './components/ProjectComponent'
+import ExperienceComponent from './components/ExperienceComponent'
 
-class App extends React.Component{
-
-
-    render() {
-        return(
-            <Router>
-                <HeaderComponent>
-                    <Link to="/"></Link>
-                </HeaderComponent>
-                <Layout style={{ minHeight: '92vh' }}>
-                    <SideBarComponent/>
-                    <Layout>
-                        <Switch>
-                            {
-                                //a way to pass a prop isAuthed to a Dashboard component when using <Route></Route>
-                                //using render prop instead of component prop will mean the component wont get 
-                                //unnecessarily remounted like with component
-                                /*
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<HeaderComponent>
+					<Link to='/'></Link>
+				</HeaderComponent>
+				<Layout style={{ minHeight: '92vh' }}>
+					<SideBarComponent />
+					<Layout>
+						<Switch>
+							{
+								//a way to pass a prop isAuthed to a Dashboard component when using <Route></Route>
+								//using render prop instead of component prop will mean the component wont get
+								//unnecessarily remounted like with component
+								/*
                                 <Route
                                     path='/dashboard'
                                     render={(props) => (
@@ -44,26 +42,24 @@ class App extends React.Component{
                                     )}
                                 />
                                 */
-
-                            }
-                            <Route path='/' exact component={HomeComponent}/>
-                            <Route path='/project/1' exact component={project1}/>
-                            <Route path='/project/2' exact component={project2}/>
-                            <Route path='/project/3' exact component={project3}/>
-                            <Route path='/project/4' exact component={project4}/>
-                            <Route path='/project/5' exact component={project5}/>
-                            <Route path='/project/6' exact component={project6}/>
-                            <Route path='/project/7' exact component={project7}/>
-                            <Route path='/experience' exact component={ExperienceComponent}/>
-                            <Route path='/resume' exact component={ResumeComponent}/>
-                        </Switch>
-                        <FooterComponent/>
-                    </Layout>
-                </Layout>   
-            </Router>
-        )
-    }
+							}
+							<Route path='/' exact component={HomeComponent} />
+							<Route path='/project/1' exact component={project1} />
+							<Route path='/project/2' exact component={project2} />
+							<Route path='/project/3' exact component={project3} />
+							<Route path='/project/4' exact component={project4} />
+							<Route path='/project/5' exact component={project5} />
+							<Route path='/project/6' exact component={project6} />
+							<Route path='/project/7' exact component={project7} />
+							<Route path='/experience' exact component={ExperienceComponent} />
+							<Route path='/resume' exact component={ResumeComponent} />
+						</Switch>
+						<FooterComponent />
+					</Layout>
+				</Layout>
+			</Router>
+		)
+	}
 }
-
 
 export default App
