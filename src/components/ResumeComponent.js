7c7a1c18d36a31974e6281d4c29fe7cbdfa107a9
Raymond Lam <43391310+rayoloo@users.noreply.github.com>
2021-11-18 22:04:29 -0500

    new goals

--- src/components/ResumeComponent.js
+++ src/components/ResumeComponent.js
@@ -1,30 +1,37 @@
 import React from 'react'
-import {Layout, Button} from 'antd'
-import { DownloadOutlined } from '@ant-design/icons';
+import { Layout, Button } from 'antd'
+import { DownloadOutlined } from '@ant-design/icons'
 import 'antd/dist/antd.css'
 
-
 export default function ResumeComponent() {
-    const {Content} = Layout
-    const resumeLink = "https://drive.google.com/u/0/uc?id=1BR-g3xiQqkBeL8NMKDqBhmsODcHQOFxc&export=download"
-    return (
-        <Content>
-            <br/>
-            <div style={{position: "absolute", right: "30px", top: "100px"}}>
-            <Button type="primary" icon={<DownloadOutlined />} size="large" href={resumeLink}>
-            Download
-            </Button>
-            </div>
-            <br/>
-            <div style={{textAlign:'center', height: "92%"}}>
-                <iframe 
-                    title="resume document"
-                    src="https://docs.google.com/document/d/e/2PACX-1vTsws3RWgMXyKdfc_Jt05oksXem4x-FyqlWDGh8SQpOUW7uUfa9V7DrtVbxw9JqRR6WTfQ--ijkdxof/pub?embedded=true"
-                    height="100%"
-                    width="50%"
-                />
-            </div>
-
-        </Content>
-    )
+	const { Content } = Layout
+	const resumeLink =
+		'https://drive.google.com/u/0/uc?id=1BR-g3xiQqkBeL8NMKDqBhmsODcHQOFxc&export=download'
+	return (
+		<Content>
+			<br />
+			<div style={{ position: 'absolute', right: '30px', top: '100px' }}>
+				<Button
+					type='primary'
+					icon={<DownloadOutlined />}
+					size='large'
+					href={resumeLink}>
+					Download
+				</Button>
+			</div>
+			<br />
+			<div style={{ textAlign: 'center', height: '92%' }}>
+				<iframe
+					title='resume document'
+					src='https://docs.google.com/document/d/e/2PACX-1vTsws3RWgMXyKdfc_Jt05oksXem4x-FyqlWDGh8SQpOUW7uUfa9V7DrtVbxw9JqRR6WTfQ--ijkdxof/pub?embedded=true'
+					height='100%'
+					width='50%'
+				/>
+			</div>
+		</Content>
+	)
 }
+
+// complete project section with more informations and maybe a gallery of screenshots
+// complete project section of resume and write a cover letter CodeOutlined
+// apply for stripe job position
